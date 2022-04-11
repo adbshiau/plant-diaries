b376a036cac7e242381aa2fe168e937e6d43f4d7
ayashiau-ga <angelicashiau@gmail.com>
2022-04-11 16:46:25 -0700

    plant model updated

--- models/plant.js
+++ models/plant.js
@@ -13,33 +13,55 @@ const noteSchema = new Schema(
   }
 );
 
-const plantSchema = new Schema({
-  commonName: String,
-  location: {
-    type: String,
-    enum: [
-      "office",
-      "hall",
-      "kitchen",
-      "bathroom",
-      "terrace",
-      "porch",
-      "balcony",
-      "living room",
-      "patio",
-      "bedroom",
-    ],
+const plantSchema = new Schema(
+  {
+    // general information
+    userOwns: {
+      type: Schema.Types.ObjectId,
+      ref: "User",
+    },
+    image: String,
+    commonName: String,
+    scientificName: String,
+    adoptionDate: Date,
+    location: {
+      type: String,
+      enum: [
+        "office",
+        "hall",
+        "kitchen",
+        "bathroom",
+        "terrace",
+        "porch",
+        "balcony",
+        "living room",
+        "patio",
+        "bedroom",
+      ],
+    },
+    toxicity: {
+      type: String,
+      enum: ["human safe", "pet safe"],
+    },
+    // plant care details
+    light: {
+      type: String,
+      enum: ["full sun", "part sun", "part shade", "full shade"],
+    },
+    water: String,
+    idealTemp: Number,
+    humidity: Number,
+    soil: String,
+    fertilizer: String,
+    propagation: {
+        type: String,
+        enum: ['seeds', 'bulbs', 'food scraps', 'cuttings', 'layering', 'grafting']
+    },
+    notes: [noteSchema]
   },
-  light: {
-    type: String,
-    enum: ["full sun", "part sun", "part shade", "full shade"],
-  },
-  userOwns: {
-    type: Schema.Types.ObjectId,
-    ref: "User",
-  },
-  notes: [noteSchema],
-  image: String
-});
+  {
+    timestamps: true,
+  }
+);
 
 module.exports = mongoose.model("Plant", plantSchema);
